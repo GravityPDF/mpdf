760c51f2c30fe51998e10feaf3bd97b3d53da1c2
Jake Jackson <admin@blueliquiddesigns.com.au>
2015-06-14 15:00:32 +1000

    Configure PHPUnit and write baseline tests for the mPDI (FPDI) classes
    Add GitIgnore
    
    Fix up spacing issue

---
+++ tests/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Setup our unit testing functionality
+ *
+ * @package    mPDF
+ * @author     Blue Liquid Designs <admin@blueliquiddesigns.com.au>
+ * @copyright  2015 Blue Liquid Designs
+ * @license    GPLv2
+ * @since      6.1.0
+ */
+
+/* Load the mPDF class */
+require_once('mpdf.php');
